0d7d19dd36ec2b916a2e5c6176837fe1a4c11477
WenqingWu <wenqingwu2015@gmail.com>
2019-01-08 21:38:36 +0800

    modified name of application (Run.sh)

--- Run.sh
+++ Run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ./build/app/streamGen-dpdk -c 0x1 -n 1 -- -i pcapfiles/dump5.pcap -o 0 -c 100 -t  2
+sudo ./build/app/streamGen -c 0x1 -n 1 -- -i pcapfiles/dump5.pcap -o 0 -c 100 -t  2
